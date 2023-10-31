bfa3bacddca1ba8d054644efb3103896ab89a481
zhewei0607 <146732595+zhewei0607@users.noreply.github.com>
2023-10-31 08:48:45 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+     int b,num,i,d;
+     int a[60];
+     scanf("%d",&num);
+     for(i=0;i<num;i++)
+     {
+           scanf("%d",&a[i]);
+     }
+     for(i=0;i<num;i++)
+     {
+           int c=2;
+           for(b=2;b<a[i];b++)
+           {
+                if((a[i]%b)==0)
+                {
+                printf("NO ");
+                c=1;
+                break;
+                }
+
+           }
+           if(c!=1)
+           {
+                printf("YES ");
+           }
+     }
+}
 
